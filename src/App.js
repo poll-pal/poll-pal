8acb4678751c3f6644f8e0e9aa4f22fca12e5223
Schuyler Ankele <schuyler.ankele@charter.com>
2018-10-10 21:13:11 -0600

    Basic Implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Countdown from './Components/Countdown/'
 
 class App extends Component {
   constructor() {
@@ -24,6 +25,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          The Final Countdown:
+        </h1>
+        <Countdown />
       </div>
     );
   }
