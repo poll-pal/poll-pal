4a4e77d27ad698e4f1d91f4ff510944a41c6165c
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-10-15 20:52:15 -0600

    Move header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
+
+const Header = props => {
+    // Setting up main navbar content
+    let content = (
+        <li className="nav-item">
+            <a className="nav-link" href="/auth/google">Login</a>
+        </li>,
+        <li className="nav-item" key="4">
+            <Link to="/survey">
+                <span className="navbar-text">Get Started</span>
+            </Link>
+        </li>
+    );
+    // Navbar content if user is logged in
+    if (props.user) {
+        content = [
+            <li className="nav-item" key="1">
+                <span className="navbar-text">Hello, {props.user.givenName}</span>
+            </li>,
+            <li className="nav-item" key="2">
+                <Link to="/user">
+                    <img
+                        style={{ borderRadius: '50%', padding: '8px 8px' }}
+                        src={props.user.imageURL}
+                        alt="User profile"
+                    />
+                </Link>
+            </li>,
+            <li className="nav-item" key="5">
+                <Link to="/survey">
+                    <span className="navbar-text">Get Started</span>
+                </Link>
+            </li>,
+            <li className="nav-item" key="3">
+                <a className="nav-link" href="/auth/logout">Logout</a>
+            </li>
+        ];
+    }
+
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+                Poll Pal
+                    </Link>
+            <ul className="navbar-nav">{content}</ul>
+        </nav>
+    );
+};
+
+export default Header;
