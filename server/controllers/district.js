3fbd1c761db2cf7168ea44acb9bada904584b56e
Schuyler Ankele <schuyler.ankele@charter.com>
2018-10-21 03:15:34 -0600

    Voter district search

---
+++ server/controllers/district.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const axios = require('axios');
+const BP_URL = `https://api.ballotpedia.org/v3/api/contains?access_token=${process.env.BP_KEY}&point=`;
+/* 
+ * 
+ */
+const getDistrictCoord = (latLng, callback = null) => {
+    let uri = BP_URL + latLng.join(',');
+    console.log(uri);
+    axios.get(uri)
+        .then(response => {
+            if(callback){
+                callback.json(response);
+            }
+            else{
+                console.log(response);
+            }
+    })
+        .catch(err => console.log(err.message));
+};
+
+const getDistrictByAddress = (address, callback = null) => {
+    // TODO - Convert address to coords
+    axios.get(BP_URL)
+        .then(response => { 
+            if(callback){
+                callback.json(response);
+            }
+            else{
+                console.log(response);
+            }
+    })
+        .catch(err => console.log(err.message));
+}
+
+module.exports = {
+    latLng: getDistrictCoord
+}
