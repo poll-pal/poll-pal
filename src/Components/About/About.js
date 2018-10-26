a3e1822d4050b17e6eec2c06c9de9609ba75abf4
Katelyn Manuel <katelynmanuel@Katelyns-MacBook-Pro.local>
2018-10-25 21:27:39 -0600

    Adding about page copy.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,7 +2,22 @@ import React from 'react'
 import "./About.css";
 
 const About = () => (
-    <h1 id="aboutTitle">Shit's Getting Real</h1>
+    <div className="container about">
+        <div className="row">
+            <h1 id="aboutTitle">Shit's Getting Real</h1>
+            <p>
+                We get it. There’s a ton to a ballot and well, with things the way they are, you don’t want to vote wrong, right?
+                That’s where we come in. Get a closer look at your state and district candidates and initiatives so you can make
+                informed decisions in every election.
+            </p>
+            <p>
+                Enter your zip code for a snapshot of your ballot, then get prepared to make a difference. Poll Pal makes it easy 
+                for you to make informed decisions so you can get involved without all the confusion and stress that comes with 
+                ballots these days.
+            </p>
+        </div>
+    </div>
+    
 )
 
 export default About;
