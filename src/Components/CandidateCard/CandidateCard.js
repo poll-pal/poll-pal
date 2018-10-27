7dc01e01608af77903ebdaea38a87c08ac979e56
Katelyn Manuel <katelynmanuel@Katelyns-MacBook-Pro.local>
2018-10-27 12:21:32 -0600

    Ballot and candidate card styling.

--- src/Components/CandidateCard/CandidateCard.js
+++ src/Components/CandidateCard/CandidateCard.js
@@ -4,30 +4,23 @@ import "./CandidateCard.css";
 
 const CandidateCard = props => (
     <div>
-
-
-        <div className="col">
+        <div className="col-lg-12 col-xs-12">
             <div className="card">
                 <div className="card-body">
                     <h3 className="card-title">{props.candidate.person_name}</h3>
-
                     {/* need to grab these */}
                     <h4>{props.candidate.race_office_name}</h4>
                     <h5>{props.candidate.part_affiliation}</h5>
                     <p>
-                        <span>{props.candidate.campaign_website_url}</span>
-                        <span> {props.candidate.campaign_twitter} | <i className="fab fa-twitter fa-sm"></i></span>
+                        <span><a href="{props.candidate.campaign_website_url}" target="_blank">website</a></span> | 
+                        <span><a href="{props.candidate.campaign_twitter}" target="_blank"><i className="fab fa-twitter fa-sm"></i></a></span>
                     </p>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <span><button type="button" className="btn btn-success">Vote Yes</button></span>&nbsp;&nbsp;<span><button type="button" className="btn btn-danger">Vote No</button></span>
-                    </div>
+                    <br/>
+                    <span><button type="button" className="btn btn-yes">Vote Yes</button></span>&nbsp;&nbsp;<span><button type="button" className="btn btn-no">Vote No</button></span>
                 </div>
             </div>
         </div>
     </div>
-
 );
 
 export default CandidateCard;
